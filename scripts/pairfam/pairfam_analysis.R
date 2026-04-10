f4ce9d1ae72219772ae60f10b0c8fb9a348fddf4
emirzeco <Emir.Zecovic@outlook.de>
2026-04-10 11:25:34 +0200

    Started analysis

--- scripts/pairfam/pairfam_analysis.R
+++ scripts/pairfam/pairfam_analysis.R
@@ -1,380 +1,224 @@
-# HH-Income ----
-## FE ----
-fe_income_hhinc <- plm(
-  satrelship ~ log_hhinc,
-  data  = p_reduc,
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# R code for                                                                              # 
+# Means-Tested Benefits and Relationship Satisfaction among Low-Income Couples in Germany #
+# Author: Emir Zecovic                                                                    #
+# Last Update: 10.04.2026                                                                 #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # #
+# ANALYIS PAIRFAM #
+# # # # # # # # # # 
+
+# FE ----
+## M1 (HHInc)----
+### M1a ----
+M1a <- plm(
+  satrelship ~ log_hhincgcee,
+  data = p_reduc,
   index = c("id", "wave"),
   model = "within"
 )
 
-fe_income_hhinc2 <- plm(
-  satrelship ~ log_hhinc + sub_fin_hh,
-  data  = p_reduc,
+### M1b ----
+M1b <- plm(
+  satrelship ~ log_hhincgcee +
+    relstat2 + lfstat + p_lfstat + 
+    nkidsliv + hlt1,
+  data = p_reduc,
   index = c("id", "wave"),
   model = "within"
 )
 
-fe_income_hhinc3 <- plm(
-  satrelship ~ log_hhinc + sub_fin_hh + lifestat + inc27i2,
-  data  = p_reduc,
+### M1c ----
+M1c <- plm(
+  satrelship ~ log_hhincgcee +
+    relstat2 + lfstat + p_lfstat + 
+    nkidsliv + hlt1 +
+    wave,
+  data = p_reduc,
   index = c("id", "wave"),
   model = "within"
 )
 
-modelsummary::msummary(
-  list(
-    "FE (1)" = fe_income_hhinc,
-    "FE (2)" = fe_income_hhinc2,
-    "FE (3)" = fe_income_hhinc3
-  ),
-  title = "HH-Income and relationship satisfaction FE (stepwise controls) [PAIRFAM]",
-  output = "hhinc_stepwise_FE_models_pairfam.html",
-  coef_map = c(
-    "log_hhinc"       = "Logged monthly household net income",
-    "sub_fin_hh"      = "Subjective household financial situation (0-10)",
-    "lifestat"        = "Life satisfaction (0-10)",
-    "inc27i2"         = "HH financial deprivation (1-5)"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared", "aic", "bic")
+### M1d ----
+M1d <- plm(
+  satrelship ~ log_hhincgcee +
+    relstat2 + lfstat + p_lfstat + 
+    nkidsliv + hlt1 +
+    reldur,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+
+### M1e ----
+M1e <- plm(
+  satrelship ~ log_hhincgcee +
+    relstat2 + reldur +
+    lfstat + p_lfstat + 
+    nkidsliv + 
+    hlt1 + 
+    wave,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+## M2 (Grundsicherung) ----
+### M2a ----
+M2a <- plm(
+  satrelship ~ benefit_dummy,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+
+### M2b ----
+M2b <- plm(
+  satrelship ~ benefit_dummy +
+    log_hhincgcee,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
 )
 
 
+### M2c ----
+M2c <- plm(
+  satrelship ~ benefit_dummy +
+    log_hhincgcee + lifesat,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
 
 
-## RE ----
-re_income_hhinc <- plm(
-  satrelship ~ log_hhinc,
-  data  = p_reduc,
+### M2d ----
+M2d <- plm(
+  satrelship ~ benefit_dummy +
+    lifesat +
+    log_hhincgcee + 
+    relstat2  +
+    lfstat + p_lfstat + 
+    nkidsliv + reldur +
+    hlt1 + 
+    wave,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+
+
+
+## M3 (Wohngeld) ----
+### M3a ----
+M3a <- plm(
+  satrelship ~ wohngeld,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+
+### M3b ----
+M3b <- plm(
+  satrelship ~ wohngeld +
+    log_hhincgcee,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+
+
+### M3c ----
+M3c <- plm(
+  satrelship ~ wohngeld +
+    log_hhincgcee + lifesat,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+
+
+### M3d ----
+M3d <- plm(
+  satrelship ~ wohngeld +
+    lifesat +
+    log_hhincgcee + 
+    relstat2  +
+    lfstat + p_lfstat + 
+    nkidsliv + reldur +
+    hlt1 + 
+    wave,
+  data = p_reduc,
+  index = c("id", "wave"),
+  model = "within"
+)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+
+# RE ----
+## M4 (HHInc) ----
+### M4a ----
+M4a <- plm(
+  satrelship ~ log_hhincgcee,
+  data = p_reduc,
   index = c("id", "wave"),
   model = "random"
 )
 
-re_income_hhinc2 <- plm(
-  satrelship ~ log_hhinc + sub_fin_hh,
-  data  = p_reduc,
+### M4b ----
+M4b <- plm(
+  satrelship ~ log_hhincgcee +
+    age + page + sex + school + migstatus + 
+    relstat2 + reldur +
+    lfstat + p_lfstat + 
+    nkidsliv + 
+    hlt1 + 
+    wave + cohort + east,
+  data = p_reduc,
   index = c("id", "wave"),
   model = "random"
 )
 
-re_income_hhinc3 <- plm(
-  satrelship ~ log_hhinc + sub_fin_hh + lifestat + inc27i2,
-  data  = p_reduc,
-  index = c("id", "wave"),
-  model = "random"
-)
 
-modelsummary::msummary(
-  list(
-    "RE (1)" = re_income_hhinc,
-    "RE (2)" = re_income_hhinc2,
-    "RE (3)" = re_income_hhinc3
-  ),
-  title = "HH-Income and relationship satisfaction RE (stepwise controls) [PAIRFAM]",
-  output = "hhinc_stepwise_RE_models_pairfam.html",
-  coef_map = c(
-    "log_hhinc"       = "Logged monthly household net income",
-    "sub_fin_hh"      = "Subjective household financial situation (0-10)",
-    "lifestat"        = "Life satisfaction (0-10)",
-    "inc27i2"         = "HH financial deprivation (1-5)"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared", "aic", "bic")
-)
-
-
 
-
-
-
-
-
-
-
-
-
-# Welfare ----
-## Any ----
-fe_welfare <- plm(
-  satrelship ~ welfare_any,
-  data  = p_reduc,
-  index = c("id", "wave"),
-  model = "within"
-)
-
-fe_welfare2 <- plm(
-  satrelship ~ welfare_any + sub_fin_hh,
-  data  = p_reduc,
-  index = c("id", "wave"),
-  model = "within"
-)
-
-fe_welfare3 <- plm(
-  satrelship ~ welfare_any + sub_fin_hh + lifestat,
-  data  = p_reduc,
-  index = c("id", "wave"),
-  model = "within"
-)
-
-fe_welfare4 <- plm(
-  satrelship ~ welfare_any + sub_fin_hh + lifestat + log_hhinc,
-  data  = p_reduc,
-  index = c("id", "wave"),
-  model = "within"
-)
-
-
-
-modelsummary::msummary(
-  list(
-    "FE (1)" = fe_welfare,
-    "FE (3)" = fe_welfare2,
-    "FE (4)" = fe_welfare3,
-    "FE (5)" = fe_welfare4
-  ),
-  title = "Welfare receipt and relationship satisfaction (stepwise controls) [PAIRFAM]",
-  output = "welfare_stepwise_models_pairfam.html",
-  coef_map = c(
-    "welfare_any"    = "Welfare recept (Grundsicherung or ALG II) (0-1)",
-    "sub_fin_hh"     = "Subjective household financial situation (0-10)",
-    "lifestat"        = "Life satisfaction (0-10)",
-    "log_hhinc"  = "Logged monthly household net income"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared", "aic", "bic")
-)
-
-
-
-
-
-
-
-
-# Grundsicherung ----
-m1 <- plm(satrelship ~ grundsich,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m2 <- plm(satrelship ~ grundsich +
-            log_hhinc,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m3 <- plm(satrelship ~ grundsich +
-            log_hhinc + sub_fin_hh,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m4 <- plm(satrelship ~ grundsich +
-            log_hhinc + sub_fin_hh + 
-            lifestat,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-
-modelsummary::msummary(
-  list(
-    "FE (1)" = m1,
-    "FE (2)" = m2,
-    "FE (3)" = m3,
-    "FE (4)" = m4
-  ),
-  title = "Grundsicherung and relationship satisfaction (stepwise FE models) [PAIRFAM]",
-  output = "grundsicherung_stepwise_models_pairfam.html",
-  coef_map = c(
-    "grundsich"      = "Grundsicherung receipt",
-    "log_hhinc"      = "Logged monthly household net income",
-    "sub_fin_hh"     = "Subjective household financial situation (0-10)",
-    "lifestat"       = "Life satisfaction (0-10)"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('+' = .10, '*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared")
-)
-
-
-
-
-
-# Sozialgeld ----
-m20 <- plm(satrelship ~ sozhilfe,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m21 <- plm(satrelship ~ sozhilfe +
-            log_hhinc,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m22 <- plm(satrelship ~ sozhilfe +
-            log_hhinc + sub_fin_hh,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m23 <- plm(satrelship ~ sozhilfe +
-            log_hhinc + sub_fin_hh + 
-            lifestat,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-modelsummary::msummary(
-  list(
-    "FE (1)" = m20,
-    "FE (2)" = m21,
-    "FE (3)" = m22,
-    "FE (4)" = m23
-  ),
-  title = "Sozialhilfe and relationship satisfaction (stepwise FE models) [PAIRFAM]",
-  output = "sozialhilfe_stepwise_models_pairfam.html",
-  coef_map = c(
-    "sozhilfe"       = "Sozialhilfe receipt",
-    "log_hhinc"      = "Logged monthly household net income",
-    "sub_fin_hh"     = "Subjective household financial situation (0-10)",
-    "lifestat"       = "Life satisfaction (0-10)"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('+' = .10, '*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared")
-)
-
-
-
-
-
-
-# AG II ----
-m5 <- plm(satrelship ~ aII,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m6 <- plm(satrelship ~ aII +
-            log_hhinc,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m7 <- plm(satrelship ~ aII +
-            log_hhinc + sub_fin_hh,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m8 <- plm(satrelship ~ aII +
-            log_hhinc + sub_fin_hh + 
-            lifestat,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-
-modelsummary::msummary(
-  list(
-    "FE (1)" = m5,
-    "FE (2)" = m6,
-    "FE (3)" = m7,
-    "FE (4)" = m8
-  ),
-  title = "Arbeitslosengeld II and relationship satisfaction (stepwise FE models) [PAIRFAM]",
-  output = "AGII_stepwise_models_pairfam.html",
-  coef_map = c(
-    "aII"            = "Arbeitslosengeld II receipt",
-    "log_hhinc"      = "Logged monthly household net income",
-    "sub_fin_hh"     = "Subjective household financial situation (0-10)",
-    "lifestat"       = "Life satisfaction (0-10)"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared")
-)
-
-
-
-
-
-
-
-# Wohngeld ----
-m9 <- plm(satrelship ~ wohngeld,
-          data = p_reduc,
-          index = c("id", "wave"),
-          model = "within"
-)
-
-m10 <- plm(satrelship ~ wohngeld +
-             log_hhinc,
-           data = p_reduc,
-           index = c("id", "wave"),
-           model = "within"
-)
-
-m11 <- plm(satrelship ~ wohngeld +
-             log_hhinc + sub_fin_hh,
-           data = p_reduc,
-           index = c("id", "wave"),
-           model = "within"
-)
-
-m12 <- plm(satrelship ~ wohngeld +
-             log_hhinc + sub_fin_hh + 
-             lifestat,
-           data = p_reduc,
-           index = c("id", "wave"),
-           model = "within"
-)
-
-
-modelsummary::msummary(
-  list(
-    "FE (1)" = m9,
-    "FE (2)" = m10,
-    "FE (3)" = m11,
-    "FE (4)" = m12
-  ),
-  title = "Wohngeld and relationship satisfaction (stepwise FE models) [PAIRFAM]",
-  output = "Wohngeld_stepwise_models_pairfam.html",
-  coef_map = c(
-    "wohngeld"            = "Wohngeld receipt",
-    "log_hhinc"           = "Logged monthly household net income",
-    "sub_fin_hh"          = "Subjective household financial situation (0-10)",
-    "lifestat"            = "Life satisfaction (0-10)"
-  ),
-  estimate  = "{estimate}{stars}",
-  stars = c('*' = .05, '**' = .01, '***' = .001),
-  fmt = 3,
-  gof_map = c("nobs", "r.squared", "adj.r.squared")
-)
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # # # #  # # # # # # # # # # # # # # #
+# # In-Work Benefits (FE) ----
+# p_employed <- p_reduc %>%
+#   filter(
+#     lfstat %in% c(
+#       "Full-time employed",
+#       "Part-time employed",
+#       "Marginal employment",
+#       "Self-employed"
+#     )
+#   )
